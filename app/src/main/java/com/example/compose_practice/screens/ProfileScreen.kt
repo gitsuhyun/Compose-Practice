01b10c1838ddf41f5827cd161684608aec2d2ed5
gitsuhyun <suhyun10201@naver.com>
2024-08-27 21:18:56 +0900

    [Feat] 마이페이지 앱바 추가

--- app/src/main/java/com/example/compose_practice/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/compose_practice/screens/ProfileScreen.kt
@@ -4,21 +4,53 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class) // AppBar에 필요한 Experimental API 사용
 @Composable
 fun ProfileScreen() {
-    Box(modifier = Modifier.fillMaxSize()){
-        Column (modifier = Modifier
-            .fillMaxSize()
-            .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "profile", fontSize = 30.sp,)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("마이페이지") },
+                actions = {
+                    // 여기에 앱바의 오른쪽에 표시할 아이콘이나 액션 버튼을 추가할 수 있습니다.
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp) // 기본 높이 설정
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Profile", fontSize = 30.sp)
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
 }
