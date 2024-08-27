b3b81b540dfd2f0ebf3dbf102fad4a18c2787d88
jeong724 <oliveyou724@gmail.com>
2024-08-27 20:38:57 +0900

    [design] 폰트 적용

--- app/src/main/java/com/example/compose_practice/ui/theme/Type.kt
+++ app/src/main/java/com/example/compose_practice/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.compose_practice.ui.theme
 
-import androidx.compose.material3.Typography
+
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -9,6 +10,74 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.compose_practice.R
 
+@Immutable
+data class GoodMoney(
+    val Title1_r: TextStyle,
+    val Title1_m: TextStyle,
+    val Title1_sb: TextStyle,
+    val Title1_b : TextStyle,
+    val Title2_r : TextStyle,
+    val Title2_m : TextStyle,
+    val Title2_sb : TextStyle,
+    val Title2_b : TextStyle,
+    val Title3_r : TextStyle,
+    val Title3_m : TextStyle,
+    val Title3_sb : TextStyle,
+    val Title3_b : TextStyle,
+    val Heading1_r : TextStyle,
+    val Heading1_m : TextStyle,
+    val Heading1_sb : TextStyle,
+    val Heading1_b : TextStyle,
+    val Heading2_r : TextStyle,
+    val Heading2_m : TextStyle,
+    val Heading2_sb : TextStyle,
+    val Heading2_b : TextStyle,
+    val Headline1_r : TextStyle,
+    val Headline1_m : TextStyle,
+    val Headline1_sb : TextStyle,
+    val Headline1_b : TextStyle,
+    val Headline2_r : TextStyle,
+    val Headline2_m : TextStyle,
+    val Headline2_sb : TextStyle,
+    val Headline2_b : TextStyle,
+    val Bn1_r : TextStyle,
+    val Bn1_m : TextStyle,
+    val Bn1_sb : TextStyle,
+    val Bn1_b : TextStyle,
+    val Bn2_r : TextStyle,
+    val Bn2_m : TextStyle,
+    val Bn2_sb : TextStyle,
+    val Bn2_b : TextStyle,
+    val Br1_r : TextStyle,
+    val Br1_m : TextStyle,
+    val Br1_sb : TextStyle,
+    val Br1_b : TextStyle,
+    val Br2_r : TextStyle,
+    val Caption2_b: TextStyle,
+    val Caption2_sb: TextStyle,
+    val Caption2_m: TextStyle,
+    val Caption2_r: TextStyle,
+    val Caption1_b: TextStyle,
+    val Caption1_sb: TextStyle,
+    val Caption1_m: TextStyle,
+    val Caption1_r: TextStyle,
+    val Label2_b: TextStyle,
+    val Label2_sb: TextStyle,
+    val Label2_m: TextStyle,
+    val Label2_r: TextStyle,
+    val Lr1_b: TextStyle,
+    val Lr1_sb: TextStyle,
+    val Lr1_m: TextStyle,
+    val Lr1_r: TextStyle,
+    val Ln1_b: TextStyle,
+    val Ln1_sb: TextStyle,
+    val Ln1_m: TextStyle,
+    val Ln1_r: TextStyle,
+    val Br2_b: TextStyle,
+    val Br2_sb: TextStyle,
+    val Br2_m: TextStyle,
+)
+
 val pretendard = FontFamily(
     Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
     Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
@@ -16,7 +85,7 @@ val pretendard = FontFamily(
     Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal)
 )
 // Set of Material typography styles to start with
-val Typography = Typography(
+val Typography = GoodMoney(
     Title1_r = TextStyle(
         fontFamily = pretendard,
         fontSize= 38.sp,
@@ -67,278 +136,215 @@ val Typography = Typography(
     fontSize= 26.sp,
     fontWeight = FontWeight.Bold,
     fontFamily = pretendard),
-Heading1_r = TextStyle(
-fontSize: 24,
-//height: 30,
-letterSpacing: -0.3104,
-fontFamily: 'PretendardRegular');
-static const Heading1_m = TextStyle(
-fontSize: 24,
-//height: 30,
-letterSpacing: -0.3104,
-fontFamily: 'PretendardMedium');
-static const Heading1_sb = TextStyle(
-fontSize: 24,
-//height: 30,
-letterSpacing: -0.3104,
-fontFamily: 'PretendardSemi');
-static const Heading1_b = TextStyle(
-fontSize: 24,
-//height: 30,
-letterSpacing: -0.3104,
-fontFamily: 'PretendardBold');
-static const Heading2_r = TextStyle(
-fontSize: 22,
-//height: 28,
-letterSpacing: -0.192,
-fontFamily: 'PretendardRegular');
-static const Heading2_m = TextStyle(
-fontSize: 22,
-//height: 28,
-letterSpacing: -0.192,
-fontFamily: 'PretendardMedium');
-static const Heading2_sb = TextStyle(
-fontSize: 22,
-//height: 28,
-letterSpacing: -0.192,
-fontFamily: 'PretendardSemi');
-static const Heading2_b = TextStyle(
-fontSize: 22,
-//height: 28,
-letterSpacing: -0.192,
-fontFamily: 'PretendardBold');
-static const Headline1_r = TextStyle(
-fontSize: 20,
-//height: 26,
-letterSpacing: -0.192,
-fontFamily: 'PretendardRegular');
-static const Headline1_m = TextStyle(
-fontSize: 20,
-//height: 26,
-letterSpacing: -0.192,
-fontFamily: 'PretendardMedium');
-static const Headline1_sb = TextStyle(
-fontSize: 20,
-//height: 26,
-letterSpacing: -0.192,
-fontFamily: 'PretendardSemi');
-static const Headline1_b = TextStyle(
-fontSize: 20,
-//height: 26,
-letterSpacing: -0.192,
-fontFamily: 'PretendardBold');
-static const Headline2_r = TextStyle(
-fontSize: 19,
-//height: 24,
-letterSpacing: 0,
-fontFamily: 'PretendardRegular');
-static const Headline2_m = TextStyle(
-fontSize: 19,
-//height: 24,
-letterSpacing: 0,
-fontFamily: 'PretendardMedium');
-static const Headline2_sb = TextStyle(
-fontSize: 19,
-//height: 24,
-letterSpacing: 0,
-fontFamily: 'PretendardSemi');
-static const Headline2_b = TextStyle(
-fontSize: 19,
-//height: 24,
-letterSpacing: 0,
-fontFamily: 'PretendardBold');
+    Heading1_r = TextStyle(
+    fontSize = 24.sp,
+   fontWeight = FontWeight.Normal,
+    fontFamily = pretendard),
+    Heading1_m = TextStyle(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily= pretendard),
+    Heading1_sb = TextStyle(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily = pretendard),
+    Heading1_b = TextStyle(
+    fontSize = 24.sp,
+   fontWeight = FontWeight.Bold,
+    fontFamily = pretendard),
+    Heading2_r = TextStyle(
+    fontSize= 22.sp,
+    fontWeight = FontWeight.Normal,
+    fontFamily = pretendard),
+    Heading2_m = TextStyle(
+    fontSize = 22.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily = pretendard),
+    Heading2_sb = TextStyle(
+    fontSize = 22.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily = pretendard),
+    Heading2_b = TextStyle(
+    fontSize = 22.sp,
+    fontWeight = FontWeight.Bold,
+    fontFamily = pretendard),
+    Headline1_r = TextStyle(
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Normal,
+    fontFamily = pretendard),
+    Headline1_m = TextStyle(
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily = pretendard),
+    Headline1_sb = TextStyle(
+    fontSize = 20.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily= pretendard),
+    Headline1_b = TextStyle(
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Bold,
+    fontFamily = pretendard),
+    Headline2_r = TextStyle(
+    fontSize = 19.sp,
+    fontWeight = FontWeight.Normal,
+    fontFamily = pretendard),
+    Headline2_m = TextStyle(
+    fontSize = 19.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily = pretendard),
+    Headline2_sb = TextStyle(
+    fontSize = 19.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily = pretendard),
+    Headline2_b = TextStyle(
+    fontSize = 19.sp,
+    fontWeight = FontWeight.Bold,
+    fontFamily = pretendard),
 
 //Bn -> Body/Normal
-static const Bn1_r = TextStyle(
-fontSize: 18,
-//height: 24,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardRegular');
-static const Bn1_m = TextStyle(
-fontSize: 18,
-//height: 24,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardMedium');
-static const Bn1_sb = TextStyle(
-fontSize: 18,
-//height: 24,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardSemi');
-static const Bn1_b = TextStyle(
-fontSize: 18,
-//height: 24,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardBold');
-static const Bn2_r = TextStyle(
-fontSize: 17,
-//height: 22,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardRegular');
-static const Bn2_m = TextStyle(
-fontSize: 17,
-//height: 22,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardMedium');
-static const Bn2_sb = TextStyle(
-fontSize: 17,
-//height: 22,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardSemi');
-static const Bn2_b = TextStyle(
-fontSize: 17,
-//height: 22,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardBold');
-
-//Br -> Body/Reading
-static const Br1_r = TextStyle(
-fontSize: 18,
-//height: 26,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardRegular');
-static const Br1_m = TextStyle(
-fontSize: 18,
-//height: 26,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardMedium');
-static const Br1_sb = TextStyle(
-fontSize: 18,
-//height: 26,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardSemi');
-static const Br1_b = TextStyle(
-fontSize: 18,
-//height: 26,
-letterSpacing: 0.0912,
-fontFamily: 'PretendardBold');
-
-static const Br2_r = TextStyle(
-fontSize: 17,
-//height: 24,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardRegular');
-static const Br2_m = TextStyle(
-fontSize: 17,
-//height: 24,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardMedium');
-static const Br2_sb = TextStyle(
-fontSize: 17,
-//height: 24,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardSemi');
-static const Br2_b = TextStyle(
-fontSize: 17,
-//height: 24,
-letterSpacing: 0.1536,
-fontFamily: 'PretendardBold');
-
-//Ln -> Label/Normal
-static const Ln1_r = TextStyle(
-fontSize: 16,
-//height: 20,
-letterSpacing: 0.232,
-fontFamily: 'PretendardRegular');
-static const Ln1_m = TextStyle(
-fontSize: 16,
-//height: 20,
-letterSpacing: 0.232,
-fontFamily: 'PretendardMedium');
-static const Ln1_sb = TextStyle(
-fontSize: 16,
-//height: 20,
-letterSpacing: 0.232,
-fontFamily: 'PretendardSemi');
-static const Ln1_b = TextStyle(
-fontSize: 16,
-//height: 20,
-letterSpacing: 0.232,
-fontFamily: 'PretendardBold');
-
-//Lr -> Label/Reading
-static const Lr1_r = TextStyle(
-fontSize: 16,
-//height: 22,
-letterSpacing: 0.232,
-fontFamily: 'PretendardRegular');
-static const Lr1_m = TextStyle(
-fontSize: 16,
-//height: 22,
-letterSpacing: 0.232,
-fontFamily: 'PretendardMedium');
-static const Lr1_sb = TextStyle(
-fontSize: 16,
-//height: 22,
-letterSpacing: 0.232,
-fontFamily: 'PretendardSemi');
-static const Lr1_b = TextStyle(
-fontSize: 16,
-//height: 22,
-letterSpacing: 0.232,
-fontFamily: 'PretendardBold');
-
-static const Label2_r = TextStyle(
-fontSize: 15,
-//height: 18,
-letterSpacing: 0.3104,
-fontFamily: 'PretendardRegular');
-static const Label2_m = TextStyle(
-fontSize: 15,
-//height: 18,
-letterSpacing: 0.3104,
-fontFamily: 'PretendardMedium');
-static const Label2_sb = TextStyle(
-fontSize: 15,
-//height: 18,
-letterSpacing: 0.3104,
-fontFamily: 'PretendardSemi');
-static const Label2_b = TextStyle(
-fontSize: 15,
-//height: 18,
-letterSpacing: 0.3104,
-fontFamily: 'PretendardBold');
-
-static const Caption1_r = TextStyle(
-fontSize: 14,
-//height: 16,
-letterSpacing: 0.4032,
-fontFamily: 'PretendardRegular');
-static const Caption1_m = TextStyle(
-fontSize: 14,
-//height: 16,
-letterSpacing: 0.4032,
-fontFamily: 'PretendardMedium');
-static const Caption1_sb = TextStyle(
-fontSize: 14,
-//height: 16,
-letterSpacing: 0.4032,
-fontFamily: 'PretendardSemi');
-static const Caption1_b = TextStyle(
-fontSize: 14,
-//height: 16,
-letterSpacing: 0.4032,
-fontFamily: 'PretendardBold');
-
-
-static const Caption2_r = TextStyle(
-fontSize: 13,
-//height: 14,
-letterSpacing: 0.4976,
-fontFamily: 'PretendardRegular');
-static const Caption2_m = TextStyle(
-fontSize: 13,
-//height: 14,
-letterSpacing: 0.4976,
-fontFamily: 'PretendardMedium');
-static const Caption2_sb = TextStyle(
-fontSize: 13,
-//height: 14,
-letterSpacing: 0.4976,
-fontFamily: 'PretendardSemi');
-static const Caption2_b = TextStyle(
-fontSize: 13,
-//height: 14,
-letterSpacing: 0.4976,
-fontFamily: 'PretendardBold');
-
+    Bn1_r = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Normal,
+    fontFamily = pretendard),
+    Bn1_m = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily = pretendard),
+    Bn1_sb = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily = pretendard),
+    Bn1_b = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Bold,
+    fontFamily= pretendard),
+    Bn2_r = TextStyle(
+        fontSize = 17.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Bn2_m = TextStyle(
+    fontSize= 17.sp,
+    fontWeight = FontWeight.Medium,
+    fontFamily = pretendard),
+    Bn2_sb = TextStyle(
+        fontSize = 17.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Bn2_b = TextStyle(
+        fontSize= 17.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Br1_r  = TextStyle(
+        fontSize= 18.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Br1_m  = TextStyle(
+        fontSize= 18.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Br1_sb  = TextStyle(
+        fontSize= 18.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Br1_b  = TextStyle(
+        fontSize= 18.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Br2_r  = TextStyle(
+        fontSize= 17.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Br2_m  = TextStyle(
+        fontSize= 17.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Br2_sb  = TextStyle(
+        fontSize= 17.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Br2_b  = TextStyle(
+        fontSize= 17.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Ln1_r  = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Ln1_m  = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Ln1_sb  = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Ln1_b  = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Lr1_r = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Lr1_m = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Lr1_sb = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Lr1_b = TextStyle(
+        fontSize= 16.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Label2_r   = TextStyle(
+        fontSize= 15.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Label2_m   = TextStyle(
+        fontSize= 15.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Label2_sb   = TextStyle(
+        fontSize= 15.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Label2_b   = TextStyle(
+        fontSize= 15.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Caption1_r  = TextStyle(
+        fontSize= 14.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Caption1_m  = TextStyle(
+        fontSize= 14.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Caption1_sb = TextStyle(
+        fontSize= 14.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Caption2_r = TextStyle(
+        fontSize= 13.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendard),
+    Caption2_m = TextStyle(
+        fontSize= 13.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendard),
+    Caption2_sb = TextStyle(
+        fontSize= 13.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = pretendard),
+    Caption2_b = TextStyle(
+        fontSize= 13.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard),
+    Caption1_b = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendard
+    )
 )
